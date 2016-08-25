ca35544fbfd62ca86579e99a438818da48895db5
Gabriel Santos <gabriel.guarulhos@hotmail.com>
2016-08-25 16:32:51 -0300

    Initial commit

---
+++ app/src/main/java/com/example/adm/integracaocomwebservices/Greeting.java
@@ -0,0 +1,17 @@
+package com.example.adm.integracaocomwebservices;
+
+/**
+ * Created by adm on 25/08/2016.
+ */
+public class Greeting {
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
